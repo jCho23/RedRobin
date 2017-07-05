370ea15f9d1e92b13b6019bc3471a55e5ee6d9f8
JuneCho23 <junecho.ucb@gmail.com>
2017-07-05 10:44:19 -0700

    regions

--- RedRobin/Tests.cs
+++ RedRobin/Tests.cs
@@ -8,89 +8,90 @@ using System.Threading;
 
 namespace RedRobin
 {
-	[TestFixture(Platform.Android)]
-	[TestFixture(Platform.iOS)]
-	public class Tests
-	{
-		IApp app;
-		Platform platform;
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
 
-		public Tests(Platform platform)
-		{
-			this.platform = platform;
-		}
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
 
-		[SetUp]
-		public void BeforeEachTest()
-		{
-			app = AppInitializer.StartApp(platform);
-			app.Screenshot("App Launched");
-		}
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            app.Screenshot("App Launched");
+        }
 
-		[Test]
-		public void AppLaunched()
-		{
-			app.WaitForElement(x => x.Css("#ext-home-create-a-meal-btn-1"), timeout: TimeSpan.FromSeconds(80));
-			app.Screenshot("First Page");
-		}
+        [Test]
+        public void AppLaunched()
+        {
+            app.WaitForElement(x => x.Css("#ext-home-create-a-meal-btn-1"), timeout: TimeSpan.FromSeconds(80));
+            app.Screenshot("First Page");
+        }
 
-		[Test]
-		public void FirstTest()
+        [Test]
+        public void FirstTest()
+        {
+            #region Web CheatSheet
+            //http://www.w3schools.com/cssref/css_selectors.asp
+            #endregion
 
-		{
-			Thread.Sleep(15000);
-			app.WaitForElement(x => x.Css("#ext-home-create-a-meal-btn-1"), timeout: TimeSpan.FromSeconds(80));
-			app.Screenshot("App Launched");
+            #region How to Query for ALL elements 
+            //app.Query(x => x.Css("body"));  
+            #endregion
 
-			app.Tap(x => x.Css("#ext-home-create-a-meal-btn-1"));
-			app.Screenshot("Tapped on 'Create a Meal' Button");
+            #region How to Query for platform-specifc elements 
+            //Android: app.Query(x => x.WebView().Invoke("getUrl"))
+            //iOS: app.Query(x => x.WebView().Invoke("request").Invoke("URL").Invoke("absoluteString"))
+            #endregion
 
-			app.Tap(x => x.Css(".x-list-item-label"));
-			app.Screenshot("Tapped on 'Finest Burger'");
+            Thread.Sleep(15000);
+            app.WaitForElement(x => x.Css("#ext-home-create-a-meal-btn-1"), timeout: TimeSpan.FromSeconds(80));
+            app.Screenshot("App Launched");
 
-			app.Tap(x => x.Css(".x-list-item-label"));
-			Thread.Sleep(2000);
-			app.Screenshot("Tapped on 'Southen Charm' Button");
+            app.Tap(x => x.Css("#ext-home-create-a-meal-btn-1"));
+            app.Screenshot("Tapped on 'Create a Meal' Button");
 
-			Thread.Sleep(2000);
-			app.Screenshot("Shows 'Sorry, No Photo Available'");
+            app.Tap(x => x.Css(".x-list-item-label"));
+            app.Screenshot("Tapped on 'Finest Burger'");
 
-			app.Tap(x => x.Css("#ext-toolbar-allergens-1"));
-			app.Screenshot("Tapped on 'Allergen Menu'");
-			Thread.Sleep(8000);
+            app.Tap(x => x.Css(".x-list-item-label"));
+            Thread.Sleep(2000);
+            app.Screenshot("Tapped on 'Southen Charm' Button");
 
-			app.Tap(x => x.Css(".top"));
-			app.Screenshot("Tapped on 'Red Robin Locations'");
+            Thread.Sleep(2000);
+            app.Screenshot("Shows 'Sorry, No Photo Available'");
 
-			app.Tap(x => x.Css(".grey-text.loc_search_text"));
-			app.Screenshot("Tapped on 'Enter Zip, Or City, State'");
-			Thread.Sleep(4000);
+            app.Tap(x => x.Css("#ext-toolbar-allergens-1"));
+            app.Screenshot("Tapped on 'Allergen Menu'");
+            Thread.Sleep(8000);
 
-			app.EnterText("94111");
-			app.Screenshot("Entered in '94111'");
+            app.Tap(x => x.Css(".top"));
+            app.Screenshot("Tapped on 'Red Robin Locations'");
 
-			app.DismissKeyboard();
-			app.Screenshot("Dismissed Keyboard");
+            app.Tap(x => x.Css(".grey-text.loc_search_text"));
+            app.Screenshot("Tapped on 'Enter Zip, Or City, State'");
+            Thread.Sleep(4000);
 
-			app.Tap(x => x.Css(".loc_search_btn"));
-			app.Screenshot("Tapped on Search Button");
+            app.EnterText("94111");
+            app.Screenshot("Entered in '94111'");
 
-			app.Back();
-			app.Screenshot("Pressed back to get back to Main Menu");
+            app.DismissKeyboard();
+            app.Screenshot("Dismissed Keyboard");
 
-			app.Back();
-			app.Screenshot("Pressed back to get back to Main Menu");
+            app.Tap(x => x.Css(".loc_search_btn"));
+            app.Screenshot("Tapped on Search Button");
 
-			//app.Tap(x => x.Css(".ui-button-text"));
-			//app.Screenshot("Tapped 'ok' to dismiss message");
+            app.Back();
+            app.Screenshot("Pressed back to get back to Main Menu");
 
-			//app.Back();
-			//app.Screenshot("Pressed back to get back to Main Menu");
-
-			//app.Back(); 
-			//app.Screenshot("Pressed back to get back to Main Menu");
-		}
-		//app.Query(x => x.Css("body"));  
-
-	}
+            app.Back();
+            app.Screenshot("Pressed back to get back to Main Menu");
+        }
+    }
 }
